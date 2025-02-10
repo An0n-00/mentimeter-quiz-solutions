ae0e6a3f0ea4118e349e953b9453f839135fc606
An0n-00 <142223573+An0n-00@users.noreply.github.com>
2025-02-10 09:12:50 +0100

    implement

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # mentimeter-quiz-solutions
+
 A website that automatically gets the solutions of your mentimeter quiz.
+
+This project is merely a proof of concept and should not be used for any malicious purposes.
+
+## How to use
+
+1. Copy the link of the mentimeter quiz you want to get the solutions of.
+
+2. Go to the website: [https://an0n-00.github.io/mentimeter-quiz-solutions/](https://an0n-00.github.io/mentimeter-quiz-solutions/) and paste the link in the input field.
+
+3. Click on the "Get solutions" button.
+
+4. The solutions will be displayed on the screen.
+
+## How it works
+
+I noticed that the mentimeter all the data of the quiz are requested on page load. This means that the solutions are also requested and can be found in the response data. This website simply gets the data of the quiz and extracts the solutions from it.
+
+## Disclaimer
+
+This project is merely a proof of concept and should not be used for any malicious purposes. I am not responsible for any misuse of this project.
+
+## License
+
+License: mentimeter-quiz-solutions by An0n-00 is [licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International](./LICENSE).
---
+++ index.js
@@ -0,0 +1,56 @@
+document.getElementById('getAnswers').addEventListener('click', async function() {
+    let mentiLink = document.getElementById('mentiLink').value;
+    let mentiSessionId = mentiLink.split('/').pop().substring(0, 10) + "?";
+    if (mentiSessionId) {
+        document.getElementById('solution').innerText = `Fetching answers for MentiSession: ${mentiSessionId}...`;
+        let data = await getData(`https://api.allorigins.win/raw?url=https://api.mentimeter.com/audience/slide-deck/${mentiSessionId}`);
+        document.getElementById('solution').innerText = '';
+        if (data.series === undefined) {
+            document.getElementById('solution').innerText = 'No questions found. Please check if this is a correct Menti link. ' + `(${data.message})`;
+        }
+        data.series.questions.forEach(question => {
+            if (question.type === 'quiz_open') {
+                let questionText = question.question;
+                let correctAnswers = question.choices;
+                let newQuestion = document.createElement('div');
+                newQuestion.innerHTML = `<h3>Question: ${questionText}</h3>`;
+                correctAnswers.forEach(answer => {
+                    newQuestion.innerHTML += `<p>Answer: ${answer.label}</p>`;
+                });
+                document.getElementById('solution').appendChild(newQuestion);
+            }
+        });
+    } else {
+        alert('Please enter a valid menti.com link.');
+    }
+});
+
+async function getData(link) {
+    return await fetch(link, {
+        headers: {
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate, br, zstd',
+            'accept-language': 'en-GB,en;q=0.8',
+            'dnt': '1',
+            'origin': 'https://www.menti.com',
+            'referer': 'https://www.menti.com/',
+            'sec-ch-ua': '"(Not(A:Brand";v="99", "Opera";v="116", "Chromium";v="131"',
+            'sec-ch-ua-full-version-list': '"(Not(A:Brand";v="99.0.0.0", "Opera";v="116", "Chromium";v="131"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-gpc': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6779.75 Safari/537.36 OPR/116.0.5207.26'
+        }
+    })
+    .then(response => {
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            alert('Failed to fetch data. Something went wrong.');
+            throw new Error('Failed to fetch data');
+        }
+    })
+    .then(data => {
+        return data;
+    });
+}
